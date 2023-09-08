2bf3d9ca95faa050ac27a3059be5ed78bddb6b4a
Sanya Anakonda <borodsh@yandex.ru>
2023-09-08 16:14:40 +0300

    message

--- main.c
+++ main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 void main(){
-    printf("Hello world\n");
-
-
+    int a, b;
+    printf("Put in the first\n");
+    scanf("%d", &a);
+    printf("Put in the second\n");
+    scanf("%d", &b);
+    int c = a + b;
+    printf("Summ of a and b =\n %d", c);
 }
